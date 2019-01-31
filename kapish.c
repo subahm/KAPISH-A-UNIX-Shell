9d763b9dcaea6ce94d91f8387cb5ce517e5e9b2d
subah <subahmehrotra@gmail.com>
2019-01-31 12:48:28 -0800

    Updating errors

--- kapish.c
+++ kapish.c
@@ -13,11 +13,11 @@
 /*
   Function Declarations for builtin shell commands:
  */
-int lsh_cd(char **args);
-int lsh_help(char **args);
-int lsh_exit(char **args);
-int setvar(char **args);
-int unsetvar(char **args);
+int kapish_cd(char **args);
+int kapish_help(char **args);
+int kapish_exit(char **args);
+int kapish_setvar(char **args);
+int kapish_unsetvar(char **args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -30,13 +30,13 @@ char *builtin_str[] = {
 };
 
 int (*builtin_func[]) (char **) = {
-  &lsh_cd,
-  &setvar,
-  &unsetvar,
-  &lsh_exit
+  &kapish_cd,
+  &kapish_setvar,
+  &kapish_unsetvar,
+  &kapish_exit
 };
 
-int lsh_num_builtins() {
+int kapish_num_builtins() {
   return sizeof(builtin_str) / sizeof(char *);
 }
 
@@ -49,32 +49,32 @@ int lsh_num_builtins() {
    @param args List of args.  args[0] is "cd".  args[1] is the directory.
    @return Always returns 1, to continue executing.
  */
-int lsh_cd(char **args)
+int kapish_cd(char **args)
 {
   if (args[1] == NULL) {
-    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+    fprintf(stderr, "kapish: expected argument to \"cd\"\n");
   } else {
     if (chdir(args[1]) != 0) {
-      perror("lsh");
+      perror("kapish: Error");
     }
   }
   return 1;
 }
 
-int setvar(char **args){
+int kapish_setvar(char **args){
   char * var = args[2];
   if(var == NULL)
     var = "";
   if(setenv(args[1], var, 1))
-    perror("lsh: Error");
+    perror("kapish: Error");
   //printf("%s\n", getenv(args[1]));
   return 1;
 }
 
-int unsetvar(char **args){
+int kapish_unsetvar(char **args){
   //printf("%s\n", getenv(args[1]));
   if(unsetenv(args[1]))
-    perror("lsh: Error");
+    perror("kapish: Error");
   //printf("%s\n", getenv(args[1]));
   return 1;
 }
@@ -84,7 +84,7 @@ int unsetvar(char **args){
    @param args List of args.  Not examined.
    @return Always returns 0, to terminate execution.
  */
-int lsh_exit(char **args)
+int kapish_exit(char **args)
 {
   return 0;
 }
@@ -126,7 +126,7 @@ void read_kapishrc(){
   @param args Null terminated list of arguments (including program).
   @return Always returns 1, to continue execution.
  */
-int lsh_launch(char **args)
+int kapish_launch(char **args)
 {
   pid_t pid;
   int status;
@@ -135,12 +135,12 @@ int lsh_launch(char **args)
   if (pid == 0) {
     // Child process
     if (execvp(args[0], args) == -1) {
-      perror("Error");
+      perror("kapish: Error");
     }
     exit(EXIT_FAILURE);
   } else if (pid < 0) {
     // Error forking
-    perror("lsh");
+    perror("kapish: Error");
   } else {
     // Parent process
     do {
@@ -156,7 +156,7 @@ int lsh_launch(char **args)
    @param args Null terminated list of arguments.
    @return 1 if the shell should continue running, 0 if it should terminate
  */
-int lsh_execute(char **args)
+int kapish_execute(char **args)
 {
   int i;
 
@@ -165,29 +165,29 @@ int lsh_execute(char **args)
     return 1;
   }
 
-  for (i = 0; i < lsh_num_builtins(); i++) {
+  for (i = 0; i < kapish_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
   }
 
-  return lsh_launch(args);
+  return kapish_launch(args);
 }
 
-#define LSH_RL_BUFSIZE 513
+#define kapish_RL_BUFSIZE 512
 /**
    @brief Read a line of input from stdin.
    @return The line from stdin.
  */
-char *lsh_read_line(void)
+char *kapish_read_line(void)
 {
-  int bufsize = LSH_RL_BUFSIZE;
+  int bufsize = kapish_RL_BUFSIZE;
   int position = 0;
   char *buffer = malloc(sizeof(char) * bufsize);
   int c;
 
   if (!buffer) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "kapish: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
@@ -207,47 +207,47 @@ char *lsh_read_line(void)
 
     // If we have exceeded the buffer, reallocate.
     if (position >= bufsize) {
-      fprintf(stderr, "lsh: error exceeding limit\n");
+      fprintf(stderr, "kapish: error exceeding limit\n");
       exit(EXIT_FAILURE);
     }
   }
 }
 
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
+#define kapish_TOK_BUFSIZE 64
+#define kapish_TOK_DELIM " \t\r\n\a"
 /**
    @brief Split a line into tokens (very naively).
    @param line The line.
    @return Null-terminated array of tokens.
  */
-char **lsh_split_line(char *line)
+char **kapish_split_line(char *line)
 {
-  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  int bufsize = kapish_TOK_BUFSIZE, position = 0;
   char **tokens = malloc(bufsize * sizeof(char*));
   char *token, **tokens_backup;
 
   if (!tokens) {
-    fprintf(stderr, "lsh: allocation error\n");
+    fprintf(stderr, "kapish: allocation error\n");
     exit(EXIT_FAILURE);
   }
 
-  token = strtok(line, LSH_TOK_DELIM);
+  token = strtok(line, kapish_TOK_DELIM);
   while (token != NULL) {
     tokens[position] = token;
     position++;
 
     if (position >= bufsize) {
-      bufsize += LSH_TOK_BUFSIZE;
+      bufsize += kapish_TOK_BUFSIZE;
       tokens_backup = tokens;
       tokens = realloc(tokens, bufsize * sizeof(char*));
       if (!tokens) {
 		free(tokens_backup);
-        fprintf(stderr, "lsh: allocation error\n");
+        fprintf(stderr, "kapish: allocation error\n");
         exit(EXIT_FAILURE);
       }
     }
 
-    token = strtok(NULL, LSH_TOK_DELIM);
+    token = strtok(NULL, kapish_TOK_DELIM);
   }
   tokens[position] = NULL;
   return tokens;
@@ -256,7 +256,7 @@ char **lsh_split_line(char *line)
 /**
    @brief Loop getting input and executing it.
  */
-void lsh_loop(void)
+void kapish_loop(void)
 {
   char *line;
   char **args;
@@ -264,9 +264,9 @@ void lsh_loop(void)
 
   do {
     printf("? ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
+    line = kapish_read_line();
+    args = kapish_split_line(line);
+    status = kapish_execute(args);
 
     free(line);
     free(args);
@@ -284,7 +284,7 @@ int main(int argc, char **argv)
   // Load config files, if any.
   // Run command loop.
   read_kapishrc();
-  lsh_loop();
+  kapish_loop();
 
   // Perform any shutdown/cleanup.
 
