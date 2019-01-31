5a5e2816d81865f5f57e4e008421fa27feb28194
subah <subahmehrotra@gmail.com>
2019-01-31 12:43:12 -0800

    Making it work

--- kapish.c
+++ kapish.c
@@ -3,207 +3,290 @@
    Reference: https://brennan.io/2015/01/16/write-a-shell-in-c/
 */
 
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include <string.h>
-#include <signal.h>
 
-#define LSH_RL_BUFSIZE 513
-#define LSH_TOK_BUFSIZE 64
-#define LSH_TOK_DELIM " \t\r\n\a"
-int pid;
+/*
+  Function Declarations for builtin shell commands:
+ */
+int lsh_cd(char **args);
+int lsh_help(char **args);
+int lsh_exit(char **args);
+int setvar(char **args);
+int unsetvar(char **args);
 
-/*void sighand(int signum){
-	int ret = kill(pid, SIGINT);
-	printf("\n");
+/*
+  List of builtin commands, followed by their corresponding functions.
+ */
+char *builtin_str[] = {
+  "cd",
+  "setenv",
+  "unsetenv",
+  "exit"
+};
 
-	if(ret == -1 ){
-		perror("ERROR");
-		return;
-	}
-}*/
+int (*builtin_func[]) (char **) = {
+  &lsh_cd,
+  &setvar,
+  &unsetvar,
+  &lsh_exit
+};
 
-int lsh_launch(char **args){
-  pid = fork();
-  if(pid == 0){
-    //if(execvp(args[0], args) == -1))
-      perror("Error");
-  }
-  else if(pid < 0)
-    perror("Error");
-  else{
-    //signal(SIGINT, sighand);
-    waitpid(pid, NULL, 0);
+int lsh_num_builtins() {
+  return sizeof(builtin_str) / sizeof(char *);
+}
+
+/*
+  Builtin function implementations.
+*/
+
+/**
+   @brief Bultin command: change directory.
+   @param args List of args.  args[0] is "cd".  args[1] is the directory.
+   @return Always returns 1, to continue executing.
+ */
+int lsh_cd(char **args)
+{
+  if (args[1] == NULL) {
+    fprintf(stderr, "lsh: expected argument to \"cd\"\n");
+  } else {
+    if (chdir(args[1]) != 0) {
+      perror("lsh");
+    }
   }
   return 1;
 }
 
-char *read_line(){
-  int size = LSH_RL_BUFSIZE;
-  char *line = malloc(sizeof(char)*size);
-  int pos = 0;
-  int c;
-
-  if(!line){
-    fprintf(stderr, "Error in reading line\n");
-    exit(EXIT_FAILURE);
-  }
-
-  while(1){
-    // Read a character
-    c = getchar();
-    if(c == EOF){
-      exit(EXIT_SUCCESS);
-    }
-    else if(c == '\n'){
-      line[pos] = '\0';
-      return line;
-    }
-    else{
-      line[pos] = c;
-    }
-    pos++;
-    if(pos >= 512){
-      fprintf(stderr, "Line characters limit exceeded.\n");
-      return NULL;
-    }
-  }
-  return line;
-}
-
-char **split_line(char *line){
-  int bufsize = LSH_RL_BUFSIZE;
-  int length = strlen(line);
-  int pos = 0;
-  char **tokens_backup;
-  char **tokens = malloc(sizeof(char *) *length);
-  if(!tokens){
-    fprintf(stderr, "Problem in tokenizing\n");
-    exit(EXIT_FAILURE);
-  }
-  *tokens = strtok(line, LSH_TOK_DELIM);
-  while(tokens[pos] != NULL){
-    tokens[pos+1] = strtok(NULL, LSH_TOK_DELIM);
-    pos++;
-
-    if(pos >= bufsize){
-    bufsize += LSH_TOK_BUFSIZE;
-      tokens_backup = tokens;
-      tokens = realloc(tokens, bufsize *sizeof(char*));
-      if(!tokens){
-        free(tokens_backup);
-        fprintf(stderr, "allocation error\n");
-        exit(EXIT_FAILURE);
-      }
-    }
-  }
-  return tokens;
-}
-
-int change_dir(char **args){
-  int dir;
-  if(args[1] == NULL){
-    fprintf(stderr, "Expected argument tp \"cd\"\n");
-  }
-  else if(strcasecmp("HOME", args[1]) == 0){
-    dir = chdir(getenv("HOME"));
-    if( dir == -1)
-      perror("Error");
-  }
-  else{
-    dir = chdir(args[1]);
-    if(dir == -1)
-      perror("Error");
-  }
+int setvar(char **args){
+  char * var = args[2];
+  if(var == NULL)
+    var = "";
+  if(setenv(args[1], var, 1))
+    perror("lsh: Error");
+  //printf("%s\n", getenv(args[1]));
   return 1;
 }
 
-int setEnvironment(char **args){
-  if(setenv(args[1], args[2], 1))
-    perror("Error");
-  return 1;
-}
-
-int unsetEnvironment(char **args){
+int unsetvar(char **args){
+  //printf("%s\n", getenv(args[1]));
   if(unsetenv(args[1]))
-    perror("Error");
+    perror("lsh: Error");
+  //printf("%s\n", getenv(args[1]));
   return 1;
 }
 
-int terminate_kapish(){
+/**
+   @brief Builtin command: exit.
+   @param args List of args.  Not examined.
+   @return Always returns 0, to terminate execution.
+ */
+int lsh_exit(char **args)
+{
   return 0;
 }
 
-int get_path(char **args){
-  if(args[1] == NULL){
-    fprintf(stderr, "Expected argument tp \"cd\"\n");
+void read_kapishrc(){
+	/* ./kapishrc will always be in the home directory - get the path */
+	char * home = getenv("HOME");
+	char * path = malloc(sizeof(char)*(strlen(home)+strlen("/.kapishrc")+1));
+	if(!path){
+		fprintf(stderr, "Problem allocating in read_kapishrc()\n");
+		exit(EXIT_FAILURE);
+	}
+	//path = memcpy(path, home, strlen(home));
+  path = home;
+
+	/* find ./kapishrc if it exists */
+	FILE * fp = fopen(strcat(path, "/.kapishrc"), "r");
+
+	/* no .kapishrc, done */
+	if(!fp){
+    fprintf(stderr, "Problem reading kapishrc()\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* array to store each line of kapishrc */
+	char line[512];
+	printf("Reading .kapishrc\n");
+	/* get and print line */
+	while(fgets(line, sizeof(line), fp)){
+		printf("? %s", line);
+	}
+  printf("\n");
+	//free(path);
+	fclose(fp);
+}
+
+/**
+  @brief Launch a program and wait for it to terminate.
+  @param args Null terminated list of arguments (including program).
+  @return Always returns 1, to continue execution.
+ */
+int lsh_launch(char **args)
+{
+  pid_t pid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("Error");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    // Parent process
+    do {
+      waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
   }
-  else{
-    char *path;
-    getcwd(path, sizeof(path));
-    printf("%s\n", path);
-  }
+
   return 1;
 }
 
-void readFile(){
-  char *path = malloc(sizeof(char)*(strlen(getenv("HOME")+strlen("/.kapishrc")+1)));
-  if(!path){
-    fprintf(stderr, "Error finding /.kapishrc\n");
-    exit(EXIT_FAILURE);
-  }
-  path = memcpy(path, getenv("HOME"), strlen(getenv("HOME")));
-  FILE *fp = fopen(strcat(path, "/.kapishrc"), "r");
-  if(!fp){
-    printf("Error: Can't find .kapishrc");
-    return;
-  }
-  char line[512];
-  printf("Reading .kapishrc\n");
-  while(fgets(line, sizeof(line), fp))
-    printf("? %s", line);
-  printf("Done reading. \n");
-  free(path);
-  fclose(fp);
-}
+/**
+   @brief Execute shell built-in or launch program.
+   @param args Null terminated list of arguments.
+   @return 1 if the shell should continue running, 0 if it should terminate
+ */
+int lsh_execute(char **args)
+{
+  int i;
 
-int execute(char **args){
-  if(args[0] == NULL){
+  if (args[0] == NULL) {
+    // An empty command was entered.
     return 1;
   }
-  else if(strcmp(args[0],"cd") == 0)
-    return change_dir(args);
-  else if(strcmp(args[0],"exit") == 0)
-    return terminate_kapish();
-  else if(strcmp(args[0],"setenv") == 0)
-    return setEnvironment(args);
-  else if(strcmp(args[0],"unsetenv") == 0)
-    return unsetEnvironment(args);
-  else if(strcmp(args[0],"pwd") == 0)
-    return get_path(args);
-  else
-    return lsh_launch(args);
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
 }
 
-void lsh_loop(void){
+#define LSH_RL_BUFSIZE 513
+/**
+   @brief Read a line of input from stdin.
+   @return The line from stdin.
+ */
+char *lsh_read_line(void)
+{
+  int bufsize = LSH_RL_BUFSIZE;
+  int position = 0;
+  char *buffer = malloc(sizeof(char) * bufsize);
+  int c;
+
+  if (!buffer) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (1) {
+    // Read a character
+    c = getchar();
+
+    if (c == EOF) {
+      exit(EXIT_SUCCESS);
+    } else if (c == '\n') {
+      buffer[position] = '\0';
+      return buffer;
+    } else {
+      buffer[position] = c;
+    }
+    position++;
+
+    // If we have exceeded the buffer, reallocate.
+    if (position >= bufsize) {
+      fprintf(stderr, "lsh: error exceeding limit\n");
+      exit(EXIT_FAILURE);
+    }
+  }
+}
+
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+/**
+   @brief Split a line into tokens (very naively).
+   @param line The line.
+   @return Null-terminated array of tokens.
+ */
+char **lsh_split_line(char *line)
+{
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token, **tokens_backup;
+
+  if (!tokens) {
+    fprintf(stderr, "lsh: allocation error\n");
+    exit(EXIT_FAILURE);
+  }
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+    tokens[position] = token;
+    position++;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens_backup = tokens;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+      if (!tokens) {
+		free(tokens_backup);
+        fprintf(stderr, "lsh: allocation error\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+/**
+   @brief Loop getting input and executing it.
+ */
+void lsh_loop(void)
+{
   char *line;
   char **args;
   int status;
+
   do {
     printf("? ");
-    line = read_line();
-    args = split_line(line);
-    status = execute(args);
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
     free(line);
     free(args);
   } while (status);
 }
 
-int main(int argc, char **argv){
-  // Load config files
-  // Run command loop
+/**
+   @brief Main entry point.
+   @param argc Argument count.
+   @param argv Argument vector.
+   @return status code
+ */
+int main(int argc, char **argv)
+{
+  // Load config files, if any.
+  // Run command loop.
+  read_kapishrc();
   lsh_loop();
-  // Perform any cleanup/shutdown
+
+  // Perform any shutdown/cleanup.
+
+  return EXIT_SUCCESS;
 }
